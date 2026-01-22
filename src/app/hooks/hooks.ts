bcc22d7cb719c509eb6c44971d02e7d8f416c160
Harini-sparkout <harinisri@sparkouttech.com>
2026-01-22 10:56:46 +0530

    Add files via upload

---
+++ src/app/hooks/hooks.ts
@@ -0,0 +1,18 @@
+import { Component,viewChild,AfterViewInit, ViewChild, ElementRef,AfterViewChecked } from '@angular/core';
+
+@Component({
+  selector: 'app-hooks',
+  imports: [],
+  templateUrl: './hooks.html',
+  styleUrl: './hooks.css',
+})
+export class Hooks {
+   @ViewChild('myInput') myInput!: ElementRef;
+   ngAfterViewInit(){
+    this.myInput.nativeElement; 
+    console.log('ngAfterViewInit called');
+   }
+   ngAfterViewChecked(){
+   // console.log('ngafterviewchecked called');
+   }
+}
