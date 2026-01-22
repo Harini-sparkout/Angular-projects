b53c98f9660ab636394ed8b12445be4a164490f7
Harini-sparkout <harinisri@sparkouttech.com>
2026-01-22 10:58:24 +0530

    Update event.ts

--- src/app/event/event.ts
+++ src/app/event/event.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges,AfterViewInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,6 +10,8 @@ import { FormsModule } from '@angular/forms';
 export class Event {
      message: string="";
      formText:string="";
+   private timer: any;
+
      onClick(){
       this.message='button clicked';
       alert('button clicked');
@@ -28,4 +30,35 @@ export class Event {
       alert('text submitted:' +this.formText);
       this.formText='';
      }
+     ngOnInit() {
+  console.log('EventComponent loaded');
+  this.timer = setInterval(() => {
+    console.log('tick'); 
+  }, 1000);
+}
+
+      private previousData: string ='hello from angular';
+   @Input() data!: string;
+ngOnChanges(changes: SimpleChanges){
+  if(changes['data']){
+    const change= changes['data'];
+    console.log('previous:',change.previousValue);
+    console.log('current:',change.currentValue);+
+    console.log('firstchange:',change.firstChange);
+  }
+}
+ ngDoCheck() {
+  if (this.data !== this.previousData) {
+    console.log('ngDoCheck detected change');
+    console.log('previous:', this.previousData);
+    console.log('current:', this.data);
+    this.previousData = this.data;
+  }
+}
+ ngOnDestroy() {
+  clearInterval(this.timer); 
+    console.log('EventComponent is about to be destroyed');
+   
+  }
+
 }
