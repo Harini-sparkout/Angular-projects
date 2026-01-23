7cb3e5e35ed33246664a07d26aaa48d1216c01be
Harini-sparkout <harinisri@sparkouttech.com>
2026-01-23 18:40:36 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,37 @@
-import { CommonModule } from '@angular/common';
-import { Component,OnInit,OnChanges, SimpleChanges,Input } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { Component,OnInit,OnChanges, SimpleChanges,Input, Directive } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Session } from './session/session';
 import { Event } from "./event/event";
+import { Highlight } from './highlight';
+import { Condition } from './condition/condition';
+import { Child } from './child/child';
 import { Hooks } from './hooks/hooks';
+import { CustomPipeNamePipe } from './custom-pipe-name-pipe';
+import { User } from './user/user';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, CommonModule, Session, Event,Hooks],
+  standalone:true,
+  imports: [ CommonModule,FormsModule, Session, Event,Highlight,Condition,Child,Hooks,CustomPipeNamePipe,User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   email :string='';
+ 
   password='';
+  parentValue = 'Hello';
+ tempValue = '';
+  finalValue = 'welcome';
+ today=new Date();
+ amount=2500;
+  updateValue() {
+    this.finalValue = this.tempValue;
 
-   
+  }
   retrievedEmail: string | null = '';
     localstorage(){
            if(this.email===''||this.password===''){
@@ -47,13 +62,20 @@ export class App {
     if(this.inputValue ==='') 
     this.previousData=this.data;
    this.data = this.inputValue;
-   this.inputValue='';
+   this.inputValue=''; 
 }
    ngOnInit(){
-    this.previousData= this.data;
+  
 
     console.log('component loaded');
    }  
  
- 
+ show = true;
+
+destroyComponent() {
+  this.show = false;
+}
+ isHidden: boolean = true;
+
+ leader="vanmathi";
 }
